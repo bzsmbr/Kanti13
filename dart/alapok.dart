92be351d6c7397dfa9333dc2f7fce5a36a9ef96b
Bálint Zsombor <balintzsombi070301@gmail.com>
2025-09-05 08:47:13 +0200

    dart 1

---
+++ dart/alapok.dart
@@ -0,0 +1,19 @@
+//nev, iranyito, telepules, utca, hsz, atlag
+import 'dart:io';
+//valtozok
+void main() {
+  String nev = "Pankix";
+  int iranyito = 6791;
+  String telepules = "Szeged";
+  String utca = "Korom utca";
+  int hazSzam = 1;
+  double atlag = 4.535231231;
+
+  print("A neve $nev.");
+  print("A lakcime: $iranyito, $telepules, $utca $hazSzam.");
+  print("$nev atlaga: ${atlag.toStringAsFixed(2)}");
+
+  print("Irja be a nevet:");
+  String? name  = stdin.readLineSync();
+  print("A megadott nev: ${name}");
+}
